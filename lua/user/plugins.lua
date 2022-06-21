7f3013e4324938a4ef018a9453444172c1d7ce3f
NivekNK <kevin.rojas.v@mail.pucv.cl>
2022-06-20 22:59:21 -0400

    Cmp for code completion

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -41,13 +41,27 @@ packer.init {
 -- Install your plugins here
 return packer.startup(function(use)
     -- Required plugins
-    use "wbthomason/packer.nvim"    -- Have packer manage itself
-    use "nvim-lua/popup.nvim"       -- An implementation of the Popup API from vim in Neovim
-    use "nvim-lua/plenary.nvim"     -- Useful lua functions used in lots of plugins
+    use "wbthomason/packer.nvim"        -- Have packer manage itself
+    use "nvim-lua/popup.nvim"           -- An implementation of the Popup API from vim in Neovim
+    use "nvim-lua/plenary.nvim"         -- Useful lua functions used in lots of plugins
     
     -- Colorscheme
-    use "cpea2506/one_monokai.nvim"
-    
+    use "cpea2506/one_monokai.nvim"     -- One Monokai colorscheme
+
+    -- cmp
+    use "hrsh7th/nvim-cmp"              -- The completion plugin
+    use "hrsh7th/cmp-buffer"            -- buffer completions
+    use "hrsh7th/cmp-path"              -- path completions
+    use "hrsh7th/cmp-cmdline"           -- cmdline completions
+    use "saadparwaiz1/cmp_luasnip"      -- snippet completions
+
+    -- Copilot
+    -- use "github/copilot.vim"
+
+    -- Snippets
+    use "L3MON4D3/LuaSnip"              --snippet engine
+    use "rafamadriz/friendly-snippets"  -- a bunch of snippets to use
+
     -- Automatically set up your configuration after cloning packer.nvim
     if PACKER_BOOTSTRAP then
         require("packer").sync()
